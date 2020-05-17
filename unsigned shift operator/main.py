5c9d89000468d98b00930b56b3da92af954bb5d1
umangag07 <52677699+umangag07@users.noreply.github.com>
2020-05-17 17:16:36 +0530

    shift >>> operator for the unsigned decimal number

--- unsigned shift operator/main.py
+++ unsigned shift operator/main.py
@@ -3,10 +3,10 @@ import complement
 import msbshift
 left_operand=int(input("Input the left operand  for the shift '>>>' operation:"))
 right_operand=int(input("Input the right operand  for the shift '>>>' operation:"))
-lis_32b=binary.convert(left_operand)      #conert decimal to 32 bit binary
-comp1s=complement.complement1s(lis_32b)   #Turning into i's complement
-comp2s=complement.complement2s(comp1s)    #Turning into 2's complement
-msb_l=msbshift.shift(right_operand,comp2s)       #msb shifting and putting zero
+lis_32b=binary.convert(left_operand)        #conert decimal to 32 bit binary
+comp1s=complement.complement1s(lis_32b)     # Turning into i's complement
+comp2s=complement.complement2s(comp1s)      #Turning into 2's complement
+msb_l=msbshift.shift(right_operand,comp2s)  #msb shifting and putting zero
 #sum
 iterate=31
 a=1
